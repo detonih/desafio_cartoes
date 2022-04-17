fba373afa31f37a27a3a84ab4d1cb18cfb43bcaa
Henrique Detoni <detonihenrique@gmail.com>
2022-04-17 10:36:32 -0300

    add fake de cartao

--- scripts/python/data_faker.py
+++ scripts/python/data_faker.py
@@ -10,33 +10,50 @@ connection_string = MYSQL_CONN_STRING + "/" + MYSQL_DATABASE
 engine = create_engine(
     connection_string
 )
-dbConnection    = engine.connect()
+dbConnection = engine.connect()
 fake = Faker()
 range_data = range(1000)
 
 def fake_associado():
     fake_associado = defaultdict(list)
-    for i in range_data:
+    for _ in range_data:
         fake_associado["nome"].append( fake.first_name() )
         fake_associado["sobrenome"].append( fake.last_name() )
         fake_associado["dt_nasc"].append( fake.date_of_birth() )
         fake_associado["email"].append( fake.email() )
 
     df_fake_associado = pd.DataFrame(fake_associado)
-
-    df_fake_associado.to_sql("associado", con=engine, index=False, if_exists='append', chunksize=1000)
+    df_fake_associado.to_sql("associado", con=engine, index=False, if_exists='replace', chunksize=1000)
 
 def fake_conta():
     fake_conta = defaultdict(list)
     account_type_fakes = ['conta_corrente', 'conta_poupaca', 'conta_salario']
-    for i in range_data:
+    for _ in range_data:
         fake_conta["tipo"].append( fake.words(1, account_type_fakes, True) )
         fake_conta["data_criacao"].append( fake.date_this_century() )
 
     df_fake = pd.DataFrame(fake_conta)
     get_pk = pd.read_sql("select id as id_associado from mov_cartoes.associado", dbConnection)
     df_fake_conta = df_fake.join(get_pk, lsuffix='_df_fake', rsuffix='_get_pk')
+    df_fake_conta.to_sql("conta", con=engine, index=False, if_exists='replace', chunksize=1000)
 
-    df_fake_conta.to_sql("conta", con=engine, index=False, if_exists='append', chunksize=1000)
+def fake_cartao():
+    fake_cartao = defaultdict(list)
+    for _ in range_data:
+        fake_cartao["num_cartao"].append( fake.credit_card_number() )
 
+    df_fake = pd.DataFrame(fake_cartao)
+    get_pk = pd.read_sql("""
+    select associado.id as id_associado_cartao,
+    concat(associado.nome, " ",associado.sobrenome) as nom_impresso,
+    conta.id as id_conta
+    from mov_cartoes.associado as associado, mov_cartoes.conta as conta
+    where associado.id = conta.id
+    """, dbConnection)
+    df_fake_cartao = df_fake.join(get_pk, lsuffix='_df_fake', rsuffix='_get_pk')
+    print(df_fake_cartao[df_fake_cartao['num_cartao'].isnull()])
+    df_fake_cartao.to_sql("cartao", con=engine, index=False, if_exists='replace', chunksize=1000)
+
+fake_associado()
 fake_conta()
+fake_cartao()
--- scripts/sql/mov_cartoes_ddl.sql
+++ scripts/sql/mov_cartoes_ddl.sql
@@ -46,7 +46,76 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mov_cartoes`.`cartao` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `num_cartao` INT NOT NULL,
+  `num_cartao` BIGINT NOT NULL,
+  `nom_impresso` VARCHAR(100) NOT NULL,
+  `id_conta` INT NOT NULL,
+  `id_associado_cartao` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  UNIQUE INDEX `num_cartao_UNIQUE` (`num_cartao` ASC) VISIBLE,
+  INDEX `id_conta_idx` (`id_conta` ASC) VISIBLE,
+  INDEX `id_associado_idx` (`id_associado_cartao` ASC) VISIBLE,
+  CONSTRAINT `id_conta`
+    FOREIGN KEY (`id_conta`)
+    REFERENCES `mov_cartoes`.`conta` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `id_associado_cartao`
+    FOREIGN KEY (`id_associado_cartao`)
+    REFERENCES `mov_cartoes`.`associado` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mov_cartoes
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mov_cartoes` ;
+USE `mov_cartoes` ;
+
+-- -----------------------------------------------------
+-- Table `mov_cartoes`.`associado`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mov_cartoes`.`associado` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `sobrenome` VARCHAR(100) NOT NULL,
+  `dt_nasc` DATE NULL,
+  `email` VARCHAR(100) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mov_cartoes`.`conta`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mov_cartoes`.`conta` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `tipo` VARCHAR(50) NOT NULL,
+  `data_criacao` TIMESTAMP NULL,
+  `id_associado` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  INDEX `id_associado_idx` (`id_associado` ASC) VISIBLE,
+  CONSTRAINT `id_associado`
+    FOREIGN KEY (`id_associado`)
+    REFERENCES `mov_cartoes`.`associado` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mov_cartoes`.`cartao`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mov_cartoes`.`cartao` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `num_cartao` BIGINT NOT NULL,
   `nom_impresso` VARCHAR(100) NOT NULL,
   `id_conta` INT NOT NULL,
   `id_associado_cartao` INT NOT NULL,
@@ -68,6 +137,30 @@ CREATE TABLE IF NOT EXISTS `mov_cartoes`.`cartao` (
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `mov_cartoes`.`movimento`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mov_cartoes`.`movimento` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `vlr_transacao` DECIMAL(10,2) NULL,
+  `des_transacao` VARCHAR(100) NULL,
+  `data_movimento` TIMESTAMP NOT NULL,
+  `id_cartao` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  INDEX `id_cartao_idx` (`id_cartao` ASC) VISIBLE,
+  CONSTRAINT `id_cartao`
+    FOREIGN KEY (`id_cartao`)
+    REFERENCES `mov_cartoes`.`cartao` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
 -- Table `mov_cartoes`.`movimento`
